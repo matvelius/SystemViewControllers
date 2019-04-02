05153c2ba544117bc0f561d5ce2e60b50bcd9241
Matvey Kostukovsky <matvelius@gmail.com>
2019-04-02 16:29:03 -0500

    initial commit with segmented control

--- SystemViewControllers/ViewController.swift
+++ SystemViewControllers/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func optionTapped(_ sender: Any) {
+        
+        switch segmentedControl.selectedSegmentIndex {
+            
+        // SHARE
+        case 0:
+            
+            guard let image = imageView.image else { return }
+            
+            let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+            
+            activityController.popoverPresentationController?.sourceView = sender as? UIView
+            
+            present(activityController, animated: true, completion: nil)
+            
+        // SAFARI
+        case 1: print("1")
+            
+        // CAMERA
+        case 2: print("2")
+            
+        // EMAIL
+        case 3: print("3")
+            
+        default: break
+        }
+    }
+    
 
 }
 
