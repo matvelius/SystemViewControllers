c32863fa39610118206f2e26f51f4737a936df57
Matvey Kostukovsky <matvelius@gmail.com>
2019-04-02 18:42:45 -0500

    add select from photo library functionality

--- SystemViewControllers/ViewController.swift
+++ SystemViewControllers/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import SafariServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     
@@ -25,11 +26,65 @@ class ViewController: UIViewController {
     }
     
     @IBAction func safariButtonTapped(_ sender: UIButton) {
+        
+        if let url = URL(string: "http://www.unsplash.com") {
+            let safariViewController = SFSafariViewController(url: url)
+            present(safariViewController, animated: true, completion: nil)
+        }
     }
     
     
     @IBAction func photosButtonTapped(_ sender: UIButton) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        let alertController = UIAlertController(title: "Choose Image Source", message: nil, preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(cancelAction)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+        
+            let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: { action in
+                
+                imagePicker.sourceType = .camera
+                self.present(imagePicker, animated: true, completion: nil)
+                
+            })
+            
+            alertController.addAction(cameraAction)
+            
+        }
+        
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+        
+            let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: { action in
+                
+                imagePicker.sourceType = .photoLibrary
+                self.present(imagePicker, animated: true, completion: nil)
+                
+            })
+            
+            alertController.addAction(photoLibraryAction)
+        
+        }
+        
+        present(alertController, animated: true, completion: nil)
+        
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let selectedImage = info[.originalImage] as? UIImage {
+            imageView.image = selectedImage
+            dismiss(animated: true, completion: nil)
+        }
+        
+    }
+    
     
     @IBAction func emailButtonTapped(_ sender: UIButton) {
     }
